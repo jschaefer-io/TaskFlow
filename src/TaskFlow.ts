3536f6cd61ed185d29cb1a69c3279c55a9ef87f4
Jannik Schäfer <me@jschaefer.io>
2018-05-05 23:40:03 +0200

    Prepare API build

--- src/TaskFlow.ts
+++ src/TaskFlow.ts
@@ -1,21 +1,31 @@
+// Extend Window Type with TaskFlow-Property
+interface TaskFlowWindow extends Window{
+    TaskFlow: any
+}
+declare const window: TaskFlowWindow;
+
+
 import Chart from "./class/Chart";
 import ChartItem from "./class/ChartItem";
 import ChartConnection from "./class/items/ChartConnection";
 import ChartAction from "./class/items/ChartAction";
 import ChartEvent from "./class/items/ChartEvent";
+import ChartLine from "./class/items/ChartLine";
+import ChartText from "./class/items/ChartText";
 
-function TaskFlow(){
-    // Test Process
-    const chart: Chart = new Chart('#test', 1000, 1000);
-    let chartAction: ChartItem = new ChartAction('TestAction', 500, 400);
-    let chartEvent: ChartItem = new ChartEvent('TestEvent', 340, 100);
-    chart.append(chartAction);
-    chart.append(chartEvent);
-    let connection: ChartItem = new ChartConnection(chartAction, chartEvent);
-    chart.append(connection);
-}
+
+const TaskFlow = {
+    Chart: Chart,
+    Item: ChartItem,
+    Items: {
+        Action: ChartAction,
+        Connection: ChartConnection,
+        Event: ChartEvent,
+        Line: ChartLine,
+        Text: ChartText
+    }
+};
+
+window.TaskFlow = TaskFlow;
 
 export default TaskFlow;
-
-// Test Execution
-TaskFlow();
